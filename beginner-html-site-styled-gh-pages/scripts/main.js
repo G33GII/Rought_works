9d754465d435717ee5eb1e3aae1a8d77c16efeb3
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 14:45:02 +0000

    Mon, Feb 12, 2024, 3:45 PM +01:00

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -5,7 +5,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
     const mySrc = myImage.getAttribute("src");
     if (mySrc === "images/firefox-icon.png") {
-        myImage.setAttribute("src", "image/firefox2.png");
+        myImage.setAttribute("src", "images/firefox2.png");
     } else {
         myImage.setAttribute("src", "images/firefox-icon.png");
     }
