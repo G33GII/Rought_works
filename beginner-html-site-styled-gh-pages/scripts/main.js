8f477b3f5309fd35c8c23d14550b2c3a9a5db107
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 15:09:54 +0100

    changes to Rought work

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,13 @@
+
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "image/firefox2.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
+
