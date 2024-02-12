838e3fd8a197183be0fae7aee967280f1b5dfa1c
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-12 19:39:00 +0000

    Mon, Feb 12, 2024, 8:39 PM +01:00

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -11,3 +11,11 @@ myImage.onclick = () => {
     }
 };
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = promt("Please enter your name: ");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+}
